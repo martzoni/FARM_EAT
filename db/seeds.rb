1dda3fb2e68ab01b6ec4bf9f6c72f3084abd7ad0
Oscar Nicoulaz <nicoulaz.oscar@hotmail.com>
2021-08-17 11:55:19 +0200

    pre merging from master

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,27 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://www.conservation-nature.fr/food/fruits/"
+# parsing for products
+# url_product = "https://www.conservation-nature.fr/food/fruits/"
 
-html_content = URI.open(url).read
-doc = Nokogiri::HTML(html_content)
+# html_content_product = URI.open(url_product).read
+# doc_product = Nokogiri::HTML(html_content_product)
 
-fruits = []
-doc.search('.int-plante-tab h3').each do |element|
-  Product.new(name: element).save
+# doc_product.search('.int-plante-tab h3').each do |element|
+#   Product.new(name: element).save
+# end
+
+# parsing for farms
+url_farm = "https://www.marchepaysan.ch/index.php/producteur"
+
+html_content_farm = URI.open(url_farm).read
+doc_farm = Nokogiri::HTML(html_content_farm)
+
+doc_farm.search('.item-name').each do |card_info|
+  # f = Farm.new()
+  # f.name = card_info.search('h3')
+  # f.address = card_info.search('.point-subtitle')
+  # f.save
+  puts card_info.search('a')['href']
+  puts card_info.search('span')[1].text
 end
