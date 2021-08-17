6f94b763f94dd7f44a710190974c176ff24fb6a4
Pablo Roleri Lopez <rolerilopez@hotmail.com>
2021-08-17 11:44:51 +0200

    Create seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 require 'nokogiri'
+require 'faker'
+
+# Start seeding
+puts "Cleaning database..."
+# Farm.destroy_all
+User.destroy_all
+Product.destroy_all
+# Groceries.destroy_all
+# Stock.destroy_all
 
 url = "https://www.conservation-nature.fr/food/fruits/"
 
@@ -16,4 +25,42 @@ doc = Nokogiri::HTML(html_content)
 fruits = []
 doc.search('.int-plante-tab h3').each do |element|
   Product.new(name: element).save
+end
+
+puts "Creating 3 users..."
+user = User.new(
+  name: "Martin",
+  email: "martin@gmail.com",
+  password: "123456",
+  is_farmer: false
+)
+user.save!
+user = User.new(
+  name: "Pablo",
+  email: "pablo@gmail.com",
+  password: "123456",
+  is_farmer: false
+)
+user.save!
+user = User.new(
+  name: "Oscar",
+  email: "oscar@gmail.com",
+  password: "123456",
+  is_farmer: false
+)
+user.save!
+
+# 
+puts "Creating 15 farmers..."
+
+15.times do
+  user_name = Faker::Name.unique.first_name
+  puts user_name
+  user = User.new(
+    name: user_name,
+    email: "#{user_name}@hotmail.com",
+    password: "123456",
+    is_farmer: true,
+    address: ["Pl. de la Navigation 3, 1006 Lausanne", "Bouveret1897 Port-Valais", "Quai Perdonnet 19, 1800 Vevey", "Quai de Cologny 1, 1223 Cologny", "12C Chemin de beree, 1010 Lausanne", "Chemin des Esserts 5, 1024 Ecublens", "Place de la Palud 2 · 1003 Lausanne", "Rue du Simplon 16, 1800 Vevey", "Case Postale 1125, 1001 Lausanne", "Ouchy 60, 1006 Lausanne", "Grand' Rue 73, 1820 Montreux", "Av. de la Gare 33, 1618 Châtel-Saint-Denis", "Rte de Lausanne 16, 1052 Le Mont-sur-Lausanne", "Pl. de l'Hôtel-de-Ville 1, 1110 Morges", "Pl. du Château 3, 1260 Nyon"].sample)
+  user.save!
 end
