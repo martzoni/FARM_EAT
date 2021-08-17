18979fc3731c02730fefc830121b7dfb5502d3b8
Oscar Nicoulaz <nicoulaz.oscar@hotmail.com>
2021-08-17 10:30:32 +0200

    seeding Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+url = "https://www.conservation-nature.fr/food/fruits/"
+
+html_content = URI.open(url).read
+doc = Nokogiri::HTML(html_content)
+
+fruits = []
+doc.search('.int-plante-tab h3').each do |element|
+  Product.new(name: element).save
+end
